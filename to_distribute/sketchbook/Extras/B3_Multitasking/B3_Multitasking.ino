0befbfbefccacee58dd92e0abe92c069119f4dae
Dave Eslinger <dave.eslinger01@gmail.com>
2023-07-21 16:11:00 -0400

    Update B3_Multitasking.ino
    Changes to use omnidrive, changes incomplete

--- to_distribute/sketchbook/Extras/B3_Multitasking/B3_Multitasking.ino
+++ to_distribute/sketchbook/Extras/B3_Multitasking/B3_Multitasking.ino
@@ -19,9 +19,10 @@ delay to finish.
 #include <Servo.h>
 #include <NewPing.h>  // Another new library, this one to use the sonic sensor
 // Option to change sonar behavior
-#define NO_ECHO 1450  // Changes value used in NewPing library when there is no sonar return.
+// #define NO_ECHO 1450  // Changes value used in NewPing library when there is no sonar return.
 
-/*  ======================= Define Constants, these cannot change ================= */
+bool TEST = true;
+/*  ========================= Define Constants, these cannot change ========================= */
 
 // IO Pins used
 const int LEDPIN = LED_BUILTIN;  // the number of the LED pin
@@ -33,23 +34,27 @@ const byte SONIC_ECHO_PIN = 38;
 
 // Parameters controlling program behavior
 // Bump behavior
-const byte FORWARD_SPEED = 100;   // Define normal speeds
-const byte BACKWARD_SPEED = 100;  // and backup/turn speed
-const int TURN_DURATION = 600;    // Turn length in milliseconds
+/* NOTE: This program uses the odrive functions in the BreadBoardBot library. Therefore, the
+   speeds are given as percentage of maximum 90-100) and durations are in SECONDS. */
+const byte FORWARD_SPEED = 75;        // Define normal speeds,
+const byte BACKWARD_SPEED = 50;       // and backup/turn speed
+const float BACKWARD_DURATION = 0.5;  //Turn length in Seconds
+const int TURN_ANGLE = 40;            // Turn angle
 
 // Sonic sensor
-const float TARGET_DISTANCE_INCHES = 7;
-const int MAX_SONIC_DISTANCE = 200;  // cm, optional, 500 cm is default
+const float TARGET_DISTANCE_INCHES = 7.;
+const int MAX_SONIC_DISTANCE = 500;  // Must be in cm, optional, 500 cm is default
 
 // Define 'ports' for motors.
 const byte LEFT_MOTOR_PORT = 3;
 const byte RIGHT_MOTOR_PORT = 1;
+const byte BACK_MOTOR_PORT = 2;
 
 /* Define servo pins */
 const byte PANSERVOPIN = 9;  // Servo 2 on AdaFruit Motor Shield
 // const byte PANSERVOPIN = 10;  // Servo 1 on AdaFruit Motor Shield
 
-/*  Declare  and initialize GLOBAL Variables  */
+/* ========================= Declare  and initialize GLOBAL Variables ========================= */
 unsigned long ledInc = 1000;  // Increment used to turn off and on the LED
 unsigned long prevLEDMillis;  // Stores last LED update/loop time
 unsigned long driveInc = 10;
@@ -59,15 +64,18 @@ unsigned long prevServoMillis;
 
 float pingDist;  // define variable to use for distance measurements
 
-int angle = 90;     // Angle servo is pointing at, 0 to left, 180 to right, 90 directly forward.
+int angle = 90;     // Angle servo is pointing at, 0 to right, 180 to left, 90 directly forward.
 int angleInc = 25;  // amount to change servo each time servoInc is exceeded
 
-float maxDist = -999.;
-int newDirection = 0;
+float maxDist = -999.;  // Maximum distance seen with sonic sensor
+int maxAngle = 90;      // Angle at which maximum distance is observed
+int newDirection = 0;   // maxAngle mapped to robots coordinates --> 180 to 0 = -90 to +90
 
 byte ledState = HIGH;  // start with the LED turned on
 
-/* ============ Create instances of motors and sonic sensor  ===========*/
+bool brake = true;  // Use brake after spin turns?
+
+/* ========================= Create instances of motors and sonic sensor  ========================= */
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -78,12 +86,13 @@ Servo panServo;
 // Create pointers to motor control objects
 Adafruit_DCMotor *motorLeft = AFMS.getMotor(LEFT_MOTOR_PORT);
 Adafruit_DCMotor *motorRight = AFMS.getMotor(RIGHT_MOTOR_PORT);
+Adafruit_DCMotor *motorBack = AFMS.getMotor(BACK_MOTOR_PORT);
 
 // Define ultrasonic sensor with trigger & echo pins and
 //   the maximum distance to be sensed.
 NewPing sonic(SONIC_TRIGGER_PIN, SONIC_ECHO_PIN, MAX_SONIC_DISTANCE);
 
-/* ======== Functions used in just this program =============== */
+/* ========================= Functions used in just this program ========================= */
 
 float Distance_inches(int ping) {
   float inches_per_sec = 13582.67;  // Equivilent to 345 m/s
@@ -101,6 +110,7 @@ void setup(void) {
   // Turn off all motors to start, just a good habit
   motorLeft->run(RELEASE);
   motorRight->run(RELEASE);
+  motorBack->run(RELEASE);
 
   panServo.attach(PANSERVOPIN);
   panServo.write(angle);
@@ -128,6 +138,12 @@ void setup(void) {
   while (digitalRead(RIGHT_BUMP_PIN)) {};
   while (!digitalRead(RIGHT_BUMP_PIN)) {};
   delay(300);  // Bump pin triggered and released, just give 0.3 seconds to get hands out of the way.
+  if (TEST) {
+    odrive(-90, 50, 2, true,
+           motorLeft, motorRight, motorBack);
+    while (1) {};
+  } else {
+  }
 }
 
 void loop() {
@@ -135,80 +151,85 @@ void loop() {
   unsigned long currentMillis = millis();  // get current time value in milliseconds.
                                            // Serial.println(String("currentMillis is " + String(currentMillis)
                                            //                       + ", previous LED Millis is " + String(prevLEDMillis)));
-  // motorLeft->setSpeed(FORWARD_SPEED);  // Slowly back up and turn to right
-  // motorRight->setSpeed(FORWARD_SPEED);
-  // motorLeft->run(FORWARD);
-  // motorRight->run(FORWARD);""
-  Serial.println("Normal drive")
 
-    if (currentMillis - prevLEDMillis >= ledInc) {  // Check if LED increment has been exceeded. If so...
-    prevLEDMillis = currentMillis;                  // Update previous value then
-    if (ledState == HIGH) {                         // Change ledState variable to other state (HIGH or LOW), and
+  // Drive forward as default behavior
+  motorLeft->setSpeed(FORWARD_SPEED);  // Slowly back up and turn to right
+  motorRight->setSpeed(FORWARD_SPEED);
+  motorLeft->run(FORWARD);
+  motorRight->run(FORWARD);
+  Serial.println("Flow test: normal drive");
+
+  if (currentMillis - prevLEDMillis >= ledInc) {  // Check if LED increment has been exceeded. If so...
+    prevLEDMillis = currentMillis;                // Update previous value then
+    if (ledState == HIGH) {                       // Change ledState variable to other state (HIGH or LOW), and
       ledState = LOW;
     } else {
       ledState = HIGH;
     }
     digitalWrite(LEDPIN, ledState);  // Write out ledState to LEDPIN
-  }                                  // end of LED section
+    Serial.println("Flow test: BLINK LED");
+  }  // end of LED section
 
   if (currentMillis - prevServoMillis >= servoInc) {  // Check if Servo increment has been exceeded. If so...
     prevServoMillis = currentMillis;                  // Update previous value then
     angle += angleInc;                                // Calculate new angle and
     if (angle <= 20 || angle >= 160) {                // Check to see if angle is at end of range if so
-      angleInc = -angleInc;                           // Change increment to opposite direction
+      angleInc = -angleInc;                           // Change increment to opposite
     }
-    // panServo.write(angle);
-    int nping = 0;
-    do {
-      pingDist = Distance_inches(sonic.ping(MAX_SONIC_DISTANCE));
-      nping++;
-      if (pingDist == 0.0) { pingDist = MAX_SONIC_DISTANCE; }
-      currentMillis = millis();  // get current time value in milliseconds.
-      Serial.println(String("Ping distance is " + String(pingDist)
-                            + " and millis " + String(currentMillis)));
-      pingDist = Distance_inches(sonic.ping());
-    } while (pingDist <= 1.0);
-    Serial.println(String("Ping distance is " + String(pingDist) + " after "
-                          + String(nping) + " pings at angle " + String(angle)
+    panServo.write(angle);
+    Serial.println("Flow test: PANNING SERVO to " + String(angle)
+                   + ", distance is " + String(pingDist));
+  }  // end of Servo section
+
+  /* ========================= Get a new distance reading ========================= */
+  int nping = 0;
+  do {
+    pingDist = Distance_inches(sonic.ping());
+    nping++;
+    currentMillis = millis();  // get current time value in milliseconds.
+    Serial.println(String("Ping distance is " + String(pingDist)
                           + " and millis " + String(currentMillis)));
-    if (pingDist > maxDist) {
-      newDirection = map(angle, 180, 0, -90, 90);
-      maxDist = pingDist;
-      Serial.println(String("New maximum distance is " + String(pingDist)
-                            + " at direction " + String(newDirection)));
+    if (pingDist == 0.0) {
+      pingDist = MAX_SONIC_DISTANCE / 2.54;
+      // Serial.println("Out of range, set pingDist to MAX_SONIC_DISTANCE in inches"
+      // + String(MAX_SONIC_DISTANCE/2.54));
     }
+  } while (pingDist <= 1.0);  // end of getting new ping section
+
+  // Serial.println(String("Ping distance is " + String(pingDist) + " after "
+  //                       + String(nping) + " pings at angle " + String(angle)
+  //                       + " and millis " + String(currentMillis)));
+
+  /* ========================= Check for new maximum pingDist ========================= */
+  if (pingDist >= maxDist) {
+    maxDist = pingDist;
+    maxAngle = angle;
+    // Serial.println(String("New maximum distance is " + String(pingDist)
+    //                       + " at direction " + String(newDirection)));
   }
 
-  // if (digitalRead(LEFT_BUMP_PIN) && digitalRead(RIGHT_BUMP_PIN)
-  //     && (pingDist > TARGET_DISTANCE_INCHES
-  //         || pingDist <= 1.0)) {
-
-  if (!digitalRead(LEFT_BUMP_PIN)) {          // the LEFT side switch was bumped
-    motorLeft->setSpeed(BACKWARD_SPEED / 3);  // Slowly back up and turn to right
-    motorRight->setSpeed(BACKWARD_SPEED);
-    motorLeft->run(BACKWARD);
-    motorRight->run(BACKWARD);
-    delay(TURN_DURATION);      // for specified duration
-    motorLeft->run(RELEASE);   // Then stop power to the motors
-    motorRight->run(RELEASE);  // and move to next section of code
-  }
-  // Then check the right sensor:
-  else if (!digitalRead(RIGHT_BUMP_PIN)) {  // the RIGHT side switch was bumped
-    motorLeft->setSpeed(BACKWARD_SPEED);    // Slowly back up and turn to left
-    motorRight->setSpeed(BACKWARD_SPEED / 3);
-    motorLeft->run(BACKWARD);
-    motorRight->run(BACKWARD);
-    delay(TURN_DURATION);      // for specified duration
-    motorLeft->run(RELEASE);   // Then stop power to the motors
-    motorRight->run(RELEASE);  // and move to next section of code
-  }
-  // It must have been the sonar sensor
-  else {
-    newDirection = map(angle, 180, 0, -90, 90);
+  /* ========================= Check the sonar sensor ========================= */
+  if (pingDist <= TARGET_DISTANCE_INCHES) {
+    newDirection = map(maxAngle, 180, 0, -90, 90);
     spin(newDirection, 75, motorLeft, motorRight);
+    Serial.println(String("Flow test: TOO close, " + String(pingDist)
+                          + "inches, spin to " + String(newDirection)));
+
     panServo.write(90);
     maxDist = -999.;
   }
-  motorLeft->run(FORWARD);
-  motorRight->run(FORWARD);
+
+  /* ========================= check the left sensor: ========================= */
+  if (!digitalRead(LEFT_BUMP_PIN)) {  // the LEFT side switch was bumped
+    Serial.println("Flow test: LEFT BUMP");
+    odrive(0, -BACKWARD_SPEED, BACKWARD_DURATION, brake,
+           motorLeft, motorRight, motorBack);
+    ospinturn(TURN_ANGLE, BACKWARD_SPEED, BACKWARD_DURATION, brake,
+              motorLeft, motorRight, motorBack);
+  }
+
+  /* ========================= check the right sensor: ========================= */
+  if (!digitalRead(RIGHT_BUMP_PIN)) {  // the RIGHT side switch was bumped
+    Serial.println("Flow test: RIGHT BUMP");
+  }
 }
